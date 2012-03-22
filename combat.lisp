9a4554501cb2988ce0a03bf9f9ddc231edfef4b2
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2012-03-22 07:22:36 -0400

    randomize damage

--- combat.lisp
+++ combat.lisp
@@ -1,5 +1,3 @@
-(setq giant (make-instance 'humanoid-body))
-
 (defun attack (attacker defender weapon &optional target)
   (let ((thing-hit (attempt-hit attacker defender target weapon)))
     (if thing-hit
@@ -18,5 +16,13 @@
   (let ((body-part (assocdr thing-hit (body-parts defender)))
         (weapon-damage (active-damage-set weapon)))
     (mapc (lambda (damage-type)
-            (incf (damage-for (damage-received body-part) damage-type) (damage-for weapon-damage damage-type)))
+            (incf (damage-for (damage-received body-part) damage-type) (random-damage (damage-for weapon-damage damage-type))))
           *damage-types*)))
+
+;; will probably end up making this more general
+(defun random-damage (base-damage)
+  (case base-damage
+    (0 0)
+    (1 1)
+    (2 (1+ (random 2)))
+    (otherwise (+ (1+ (floor (/ base-damage 2))) (random (ceiling (/ base-damage 2))))))))
