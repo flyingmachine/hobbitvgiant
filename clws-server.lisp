9022a46642fa56068be2a08a4d0fe56fc130c1ad
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2012-05-11 23:30:37 -0400

    updated to use the middleman port

--- clws-server.lisp
+++ clws-server.lisp
@@ -33,7 +33,7 @@
                                 :name "hvg server")
   (clws:register-global-resource "/game"
                                  (make-instance 'hvg-resource)
-                                 (clws:origin-prefix "http://127.0.0.1:4242" "http://localhost:4242"))
+                                 (clws:origin-prefix "http://127.0.0.1:4567" "http://localhost:4567"))
   (bordeaux-threads:make-thread (lambda ()
                                   (clws:run-resource-listener
                                    (clws:find-global-resource "/game")))
